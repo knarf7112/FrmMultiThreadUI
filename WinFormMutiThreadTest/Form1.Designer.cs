2fad41c8599e99cb7d10da64d1c37361a1931b2f
knarf7112 <knarf7112@gmail.com>
2014-11-25 10:27:23 +0800

    #0001: FrmMultiThread專案建立

---
+++ WinFormMutiThreadTest/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WinFormMutiThreadTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(12, 177);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(25, 12);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Path";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(43, 174);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(229, 22);
+            this.txtPath.TabIndex = 1;
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(14, 13);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(33, 12);
+            this.lbResult.TabIndex = 2;
+            this.lbResult.Text = "label1";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(197, 202);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "Go";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(129, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(284, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Windows Form UI 優化入門課  –  實作非同步作業";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(284, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "可以取消，又可即時掌握處理進度";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
+            this.Text = "Path";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WinFormMutiThreadTest/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinFormMutiThreadTest
+{
+    public partial class Form1 : Form
+    {
+        private int fileCount = 0;
+        private long totalSize = 0;
+        private bool cancel = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPath.Text = Path.GetTempPath();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //每隔50ms顯示最新時間於狀態列
+            toolStripStatusLabel1.Text = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            ////重設統計數據
+            //fileCount = 0;
+            //totalSize = 0;
+            ////統計檔案數及大小
+            //explore(txtPath.Text);
+            ////顯示結果
+            //lbResult.Text = string.Format("{0:#,##0} files, {1:#,##0} bytes", fileCount, totalSize);
+            //--------------------------------------------上面是舊的(同步方式)------------------------------------------------------
+            if (btnOK.Text == "Go")
+            {
+                //將統計作業排入ThreadPool Queue
+                //windows會另開Thread處理
+                //UI Thread可以繼續處理UI畫面並與使用者互動
+                ThreadPool.QueueUserWorkItem(new WaitCallback(startJob), txtPath.Text);//不給跨其他執行緒跑
+                //在 Windows Form 的世界裡，有一條鐵律:
+                //"Thou shall not access UI controls from a thread other than the one that created the UI control in the first place"
+                //“除了建立 UI Control 的那條執行緒外，你不可以用其他執行緒去存取該 UI Control
+                btnOK.Text = "Cancel";// Go ==變成==> Cancel
+                cancel = false;
+            }
+            else
+            {
+                cancel = true;
+                btnOK.Enabled = false;
+            }
+        }
+
+        delegate void UpdateLabelHandler(string text);//委派改寫狀態時間
+        //透過Invoke，printReulst會以UI Thread執行
+        //故可隨意存取UI Control
+        public void PrintResult(string text)
+        {
+            lbResult.Text = text;
+            //作業完成後canel按鈕轉回Go   Cancel ==轉成==> Go
+            btnOK.Text = "Go";
+            btnOK.Enabled = true;
+            txtPath.Text = Path.GetTempPath();
+        }
+        private void startJob(object path)
+        {
+            //重設統計數據
+            fileCount = 0;
+            totalSize = 0;
+            //統計檔案數及大小
+            explore(path.ToString());
+            //顯示結果
+            //lbResult.Text = string.Format("{0:#,##0} files, {1:#,##0} bytes", fileCount, totalSize);
+
+            //所以透過Invoke,強制UI Thread執行
+            string text = (!cancel) ? string.Format("{0:#,##0} files, {1:#,##0} bytes", fileCount, totalSize):"Canceled!!!";
+            this.Invoke(
+                new UpdateLabelHandler(PrintResult),//使用委派調用PrintResult方法來顯示遞迴完畢的資料
+                new object[] { text }
+                );
+        }
+
+        private void explore(string path)
+        {
+            //使用遞迴搜尋所有目錄
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                if (cancel) return;//如果按取消則跳離
+                explore(dir);
+            }
+            foreach (string file in Directory.GetFiles(path))
+            {
+                if (cancel) return;//if canceled leave this loop
+                Interlocked.Increment(ref fileCount);//Interlocked.Increment增加fileCounter值的做法//防止多個Thread同時更動資料時發生衝突//fileCount++;
+                //每100個檔案回報一次
+                if (fileCount % 100 == 0)
+                {
+                    this.Invoke(
+                        new UpdateLabelHandler(UpdateProcess),
+                        string.Format("{0:#,##0} files processed ...",fileCount)
+                        );
+                    Thread.Sleep(2000);
+                }
+                //加總檔案大小
+                FileInfo fi = new FileInfo(file);
+                totalSize += fi.Length;
+            }
+        }
+        private void UpdateProcess(string text)
+        {
+            txtPath.Text = text;
+        }
+    }
+}
